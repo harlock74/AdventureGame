70e7f9a3ecbe3adac1dcfa179487ba4d2427a8c6
harlock74 <albywiller@gmail.com>
2024-10-13 17:04:38 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -25,8 +25,8 @@ typedef struct{
 
 void initRooms(Room rooms[ ]){
 	// Room 0: Entrance Hall
-    //strcpy(rooms[0], "Entrance Hall");
-	rooms[0].name = "Entrance Hall";
+    //strcpy(rooms[0], "Entrance Hall");r
+    rooms[0].name = "Entrance Hall";
     rooms[0].north = 1;
     rooms[0].south = -1;
     rooms[0].east = 2;
@@ -34,7 +34,7 @@ void initRooms(Room rooms[ ]){
 
     // Room 1: Throne Room
     //strcpy(rooms[1].name, "Throne Room");
-	rooms[1].name = "Throne Room";
+    rooms[1].name = "Throne Room";
     rooms[1].north = 4;
     rooms[1].south = 0;
     rooms[1].east = -1;
@@ -43,7 +43,7 @@ void initRooms(Room rooms[ ]){
     // Room 2: Dining Room
     //strcpy(rooms[2].name, "Dining Room");
     rooms[2].name = "Dining Room";
-	rooms[2].north = 3;
+    rooms[2].north = 3;
     rooms[2].south = -1;
     rooms[2].east = 7;
     rooms[2].west = 0;
@@ -51,7 +51,7 @@ void initRooms(Room rooms[ ]){
     // Room 3: Kitchen
     //strcpy(rooms[3].name, "Kitchen");
     rooms[3].name = "Kitchen";
-	rooms[3].north = -1;
+    rooms[3].north = -1;
     rooms[3].south = 2;
     rooms[3].east = -1;
     rooms[3].west = -1;
@@ -59,7 +59,7 @@ void initRooms(Room rooms[ ]){
     // Room 4: Balcony
     //strcpy(rooms[4].name, "Balcony");
     rooms[4].name = "Balcony";
-	rooms[4].north = -1;
+    rooms[4].north = -1;
     rooms[4].south = 1;
     rooms[4].east = 6;
     rooms[4].west = -1;
@@ -67,7 +67,7 @@ void initRooms(Room rooms[ ]){
     // Room 5: Armory
     //strcpy(rooms[5].name, "Armory");
     rooms[5].name = "Armory";
-	rooms[5].north = -1;
+    rooms[5].north = -1;
     rooms[5].south = -1;
     rooms[5].east = 1;
     rooms[5].west = -1;
@@ -75,7 +75,7 @@ void initRooms(Room rooms[ ]){
     // Room 6: Library
     //strcpy(rooms[6].name, "Library");
     rooms[6].name = "Library";
-	rooms[6].north = -1;
+    rooms[6].north = -1;
     rooms[6].south = -1;
     rooms[6].east = -1;
     rooms[6].west = 4;
@@ -83,7 +83,7 @@ void initRooms(Room rooms[ ]){
     // Room 7: Secret Chamber
     //strcpy(rooms[7].name, "Secret Chamber");
     rooms[7].name = "Secret Chamber";
-	rooms[7].north = -1;
+    rooms[7].north = -1;
     rooms[7].south = -1;
     rooms[7].east = -1;
     rooms[7].west = 2;
